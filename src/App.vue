77e3f9affeb97f9606a7b18441ba069360e076c5
Bianca <bianca.rocha1711@gmail.com>
2019-08-05 09:34:21 -0300

    creating the first components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
+    <TheHeader />
     <router-view />
+    <TheFooter />
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader';
+import TheFooter from '@/components/TheFooter';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  }
+}
+</script>
+
 
 <style lang="scss">
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/"></router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style>
+</style>
