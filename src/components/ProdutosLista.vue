2a856bb7ceb201ff848b4336559292be7b48fcec
Bianca <bianca.rocha1711@gmail.com>
2019-08-06 09:36:44 -0300

    products list and  a fake api

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>produtos lista</div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
+  name: 'ProdutosLista',
+  data() {
+    return {
+      produtos: null,
+    }
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto").then(response => response.json()).then(response => {
+        this.produtos = response;
+      })
+    }
+  },
+  created() {
+    this.getProdutos();
 
+  }
 }
 </script>
 
