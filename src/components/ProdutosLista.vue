910a6ee1e886126e7a823df7006531aac3c7ac05
Bianca <bianca.rocha1711@gmail.com>
2019-08-07 08:57:40 -0300

    instaling and using axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import { api } from '@/services.js';
+
 export default {
   name: 'ProdutosLista',
   data() {
@@ -23,8 +25,8 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto").then(response => response.json()).then(response => {
-        this.produtos = response;
+      api.get("/produto").then(response => {
+        this.produtos = response.data;
       })
     }
   },
