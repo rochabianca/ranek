e5fdffdad5229c445545112da34bba02ff2713f9
Bianca <bianca.rocha1711@gmail.com>
2019-08-07 09:37:23 -0300

    styling the products list

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,14 +1,23 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0].titulo"
-      />
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p>{{ produto.descricao }}</p>
+    <div v-if="produtos && produtos.length > 0" class="produtos">
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0].src"
+            :alt="produto.fotos[0].titulo"
+          />
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">
+        Busca sem resultados. Tente buscar outro termo
+      </p>
     </div>
   </section>
 </template>
@@ -50,5 +59,49 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: white;
+  border-radius: 4px;
+  transition: all 0.2s;
+
+  &:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+  }
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  font-weight: bold;
+  color: #e80;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
+
