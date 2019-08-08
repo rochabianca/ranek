63aae6d848992c6cb80c24a02f493fcaefd2d422
Bianca <bianca.rocha1711@gmail.com>
2019-08-08 09:04:50 -0300

    styling the search input

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,6 +1,12 @@
 <template>
   <form>
-    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input
+      name="busca"
+      id="busca"
+      type="text"
+      placeholder="Buscar..."
+      v-model="busca"
+    />
     <input
       type="submit"
       id="lupa"
@@ -26,5 +32,35 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+
+  &:focus,
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
 </style>
+
