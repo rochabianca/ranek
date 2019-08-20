38798ea025d5523e6ef2dd49453c055750ce372f
Bianca <bianca.rocha1711@gmail.com>
2019-08-20 09:08:03 -0300

    creating and logging a user

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -6,7 +6,9 @@
         Criar Conta
       </button>
       <UsuarioForm v-else>
-        <button class="btn btn-form">Criar Usuário</button>
+        <button class="btn btn-form" @click.prevent="criarUsuario">
+          Criar Usuário
+        </button>
       </UsuarioForm>
     </transition>
   </section>
@@ -22,6 +24,17 @@ export default {
   data() {
     return {
       criar: false,
+    }
+  },
+  methods: {
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch('criarUsuario', this.$store.state.usuario).then(() => { })
+        await this.$store.dispatch('getUsuario', this.$store.state.usuario.email);
+        this.$router.push({ name: "usuario" })
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
