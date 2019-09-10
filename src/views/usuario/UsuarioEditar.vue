762a241f1eddeebaf09a5878c16192f55f5e21d2
Bianca <bianca.rocha1711@gmail.com>
2019-09-10 09:34:32 -0300

    updating the user

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>Editar</div>
+  <section>
+    <UsuarioForm>
+      <button @click.prevent="atualizarUsuario" class="btn">
+        Atualizar Usuário
+      </button>
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
-export default {
+import UsuarioForm from '@/components/UsuarioForm';
+import { api } from '@/services';
 
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api.put(`/usuario/${this.$store.state.usuario.id}`, this.$store.state.usuario).then(() => {
+        this.$store.dispatch("getUsuario");
+        this.$router.push({ name: "usuario" })
+      }).catch(error => {
+        console.log(error.response);
+      })
+    }
+  }
 }
 </script>
 
